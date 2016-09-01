9073851dfa56173a083291508bd49dee120d3b91
rahulkgupta <rahulkartgupta@gmail.com>
2016-08-31 18:09:59 -0700

    Increased the height for better display

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var margin = {top: 0, right: 20, bottom: 20, left: 20},
     width = 960 - margin.left - margin.right,
-    height = 180 - margin.top - margin.bottom;
+    height = 400 - margin.top - margin.bottom;
 
 var x = d3.scaleLinear()
     .domain([0, 24])
